d91134086b8a036c3767190b9de17162bde2a72e
hiskiaweb <42334639+hiskiaweb@users.noreply.github.com>
2018-08-13 17:44:03 +0700

    Update multy.js

--- multy.js
+++ multy.js
@@ -19,13 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2017/08/car-production-fall-hits-uk-industrial.html"}
-if(x=="2"){xxx="/2017/08/tesla-car-battery-production-releases.html"}
-if(x=="3"){xxx="/2017/08/green-lunacy-fossil-fuel-mandates-to.html"}
-if(x=="4"){xxx="/2017/08/us-climate-report-edits-out-highly_28.html"}
-if(x=="5"){xxx="/2017/08/arctic-melt-season-changes-and-arctic.html"}a_to_ve[i].href="https://convert-safelink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2018/08/forex-trading-what-hype-is-all-about.html"}
+if(x=="2"){xxx="/2018/08/practicing-in-forex-market.html"}
+if(x=="3"){xxx="/2018/08/the-world-wide-forex-market.html"}
+if(x=="4"){xxx="/2018/08/foreign-exchange-market-is-different.html"}
+if(x=="5"){xxx="/2018/08/who-is-participating-in-forex-market.html"}
+if(x=="6"){xxx="/2018/08/forex-trading-what-hype-is-all-about.html"}a_to_ve[i].href="https://www.forexfly.me"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
